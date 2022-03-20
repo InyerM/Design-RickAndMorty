f5dd96f10e0b2c2bb95fca40b36d620a0b37eedd
Inyer <inyermedina10@gmail.com>
2022-03-20 16:23:54 -0500

    validations

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const fillTable = () => {
 
     let num = 0
 
+    let invalid = document.querySelectorAll(".invalid")
     let tbody = document.querySelector(".body-table")
     let character = document.querySelector("#character")
     let status = document.querySelector("#status")
@@ -9,25 +10,45 @@ const fillTable = () => {
     let species = document.querySelector("#species")
     let origin = document.querySelector("#origin")
 
-    fetch("https://rickandmortyapi.com/api/character")
-    .then(res => res.json())
-    .then(data => {
+    if(character.value == "" || status.value == "Choose status" || gender.value == "Choose gender" || species.value == "" || origin.value == ""){
+        validate(character, invalid[0])
+        validate(status, invalid[1])
+        validate(gender, invalid[2])
+        validate(species, invalid[3])
+        validate(origin, invalid[4])
+    }
+    else{
+        fetch("https://rickandmortyapi.com/api/character")
+        .then(res => res.json())
+        .then(data => {
 
-        let rowData = data.results.map(item => {
-            if(character.value == item.nam || status.value == item.status || species.value == item.species || gender.value == item.gender || origin.value == item.origin) {
-                num++
-                return `<tr>
-                <td>${num}</td>
-                <td>${item.name}</td>
-                <td>${item.status}</td>
-                <td>${item.species}</td>
-                <td>${item.gender}</td>
-                <td>${item.origin.name}</td>
-                </tr>`
-            }
-        }).join('')
-        
-        tbody.innerHTML = rowData
-    })
+            let rowData = data.results.map(item => {
+                if(character.value == item.name || status.value == item.status || species.value == item.species || gender.value == item.gender || origin.value == item.origin) {
+                    num++
+                    return `<tr>
+                    <td>${num}</td>
+                    <td>${item.name}</td>
+                    <td>${item.status}</td>
+                    <td>${item.species}</td>
+                    <td>${item.gender}</td>
+                    <td>${item.origin.name}</td>
+                    </tr>`
+                }
+            }).join('')
+            
+            tbody.innerHTML = rowData
+        })
+    }
 
+}
+
+const validate = (field, invalid) => {
+    if(field.value == "" || field.value == "Choose status" || field.value == "Choose gender"){
+        field.classList.toggle("invalid-option")
+        invalid.innerHTML = "Please fill out this field"
+    }
+    else{
+        field.classList.remove("invalid-option")
+        invalid.innerHTML = ""
+    }
 }
